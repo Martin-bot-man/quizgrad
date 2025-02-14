13c0822a70934b10643d8fa9a636914b34c79889
Martin Odongo <martinwere14@gmail.com>
2025-02-14 04:33:08 +0300

    first commit

---
+++ src/pages/ChooseFavoritetopic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChooseFavoritetopic = () => {
+  return (
+    <div>ChooseFavoritetopic</div>
+  )
+}
+
+export default ChooseFavoritetopic
---
+++ src/pages/Scorescreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/NavBar'
+
+const Scorescreen = () => {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Scorescreen
