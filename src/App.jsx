a3e4ba25c75173d50605b22ac67a009b4423dc71
Martin Odongo <martinwere14@gmail.com>
2025-02-15 04:00:20 +0300

    fixed the quiz started page by simple importation into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/LandingPage'
+import QuizStarted from './pages/QuizStarted'
 
 function App() {
 
